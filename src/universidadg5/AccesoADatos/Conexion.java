b132ed40406a4ad251cafa8955c00c80c49e5be3
FranDorrego <francodorrego@gmail.com>
2023-09-07 17:05:01 -0300

    add Conexion Class
    Añado la clase conexion

---
+++ src/universidadg5/AccesoADatos/Conexion.java
@@ -0,0 +1,41 @@
+
+package universidadg5.AccesoADatos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    
+    private static final String URL = "jdbc:mariadb://localhost";
+    private static final String DB = "universidadg5";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    
+    private static Connection connection;
+
+    private Conexion() {
+    }
+    
+    public static Connection getConexion(){
+        
+        if (connection == null){
+            try {
+                
+                //Carga el driver
+                Class.forName("org.mariadb.jdbc.Driver");
+                
+                //Crea la conexion
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage() + " Error al conectarse");
+            } catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage() + " Error al cargar los drivers");
+            }
+        }
+        
+        return connection;
+        
+    }
+    
+}
