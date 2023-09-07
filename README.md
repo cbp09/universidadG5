23b3f17474d92381a19aafa8678359a6ab46d6b9
cbp09 <cbp000809@gmail.com>
2023-09-07 16:30:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# universidadG5
+ 
---
+++ src/universidadg5/AccesoADatos/UniversidadG5.java
@@ -0,0 +1,8 @@
+package universidadg5.AccesoADatos;
+public class UniversidadG5 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
